86aa175d8f86849581cf6c54400d9f092353ca4d
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-12-15 18:01:23 -0500

    js-tdd-types-of-tests/
    js-tdd-types-of-tests/

---
+++ test.js
@@ -0,0 +1,7 @@
+describe("currentAgeForBirthYear", () => {
+  it("returns the age of a person based on the year of birth", () => {
+    const birthYear = 1984;
+    const ageOfPerson = currentAgeForBirthYear(birthYear);
+    expect(ageOfPerson).toBe(38);
+  });
+});
